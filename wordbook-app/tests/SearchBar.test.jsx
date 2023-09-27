201bcb923e4d7bb8d6a5c0aa4f35c9866395de02
joakimfr <fritzjoakim@hotmail.com>
2023-09-27 10:15:41 +0200

    trying yaml

--- wordbook-app/tests/SearchBar.test.jsx
+++ wordbook-app/tests/SearchBar.test.jsx
@@ -1,4 +1,4 @@
-import SearchBar from "./SearchBar";
+import SearchBar from "../src/components/SearchBar/SearchBar";
 import { render, screen, waitFor, fireEvent } from '@testing-library/react';
 import userEvent from "@testing-library/user-event";
   
